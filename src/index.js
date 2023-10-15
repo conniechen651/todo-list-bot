de7adf0f7089c32a8f46fc4640be1f22aeee4a5a
Connie <138333893+conniechen651@users.noreply.github.com>
2023-10-14 18:37:45 -0700

    Add Node and RunningList classes
    The Node class creates an object that contains the task and due date.
    
    The RunningList class is a linked list of nodes that can add and remove
    nodes from the list.

--- src/index.js
+++ src/index.js
@@ -14,4 +14,52 @@ client.on("ready", (c) => {
     console.log(`${c.user.tag} is online.`);
 });
 
+class Node {
+    constructor(task, dueDate){
+        this.task = task;
+        this.dueDate = dueDate;
+        this.next = null;
+    }
+}
+
+class RunningList{
+    constructor(head = null){
+        this.head = head;
+    }
+
+    addFirstNode(node){
+        this.head = node;
+    }
+
+    addNode(task, dueDate){
+        const node = new Node(task, dueDate);
+        let curr;
+        if(this.head === null){
+            this.head = node;
+        } else {
+            curr = this.head;
+            while(curr.next){
+                    curr = curr.next;
+                }
+            curr.next = node;
+        }
+    }
+
+    removeNode(task){
+        let curr = this.head;
+        let lastVisited = null;
+        while(curr != null){
+            if(curr.task === task){
+                if(lastVisited == null){
+                    this.head = curr.next;
+                } else {
+                    lastVisited.next = curr.next;
+                }
+            }
+            lastVisited = curr;
+            curr = curr.next;
+        }
+    }
+}
+
 client.login(process.env.TOKEN);
